c515f0097a9e7b08ae27e633caea9df134e57aff
Abhijithabii <abhijithrajeevan123@gmail.com>
2024-04-03 15:52:37 +0530

    allowed host added

--- scraping/settings.py
+++ scraping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t*vio&6yz24hbr1$x-s8_=)g=wx6gd!1cd&!(@3kov2bbggtt+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
